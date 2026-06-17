4b7eec1541e53ea78f1c1442e267e9b8ca73e341
Justin Vu <anhvt0124@email.com>
2026-06-17 12:13:41 +1000

    feat: initial multi-env infra setup

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -42,7 +42,7 @@ resource "azurerm_container_registry" "acr" {
     name = "${var.project_name}${var.environment}acr"
     resource_group_name = azurerm_resource_group.main.name
     location = azurerm_resource_group.main.location
-    sku = "Basic
+    sku = "Basic"
     admin_enabled = false
     tags = locals.tags
 }
